a989f09198ea4d22df3b84bc44f6869d1fd9d14f
Jonathan Fielding <j.fielding@me.com>
2019-09-11 23:25:07 +0100

    chore: fix formatter to run against all code

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -4,164 +4,162 @@ import { Item, GildedRose } from '../app/gilded-rose';
 const backstagePass = 'Backstage passes to a TAFKAL80ETC concert';
 const sulfuras = 'Sulfuras, Hand of Ragnaros';
 
-describe('Gilded Rose', function () {
-    // Test for no items being added to gilded rose
-    it('should have no items in an empty shop', function() {
-        const gildedRose = new GildedRose();
-        const items = gildedRose.updateQuality();
-        expect(items.length).to.equal(0);
-    });
+describe('Gilded Rose', function() {
+  // Test for no items being added to gilded rose
+  it('should have no items in an empty shop', function() {
+    const gildedRose = new GildedRose();
+    const items = gildedRose.updateQuality();
+    expect(items.length).to.equal(0);
+  });
 
-    // Test for generic item 'foo'
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('foo');
-        expect(items[0].quality).to.equal(0);
-        expect(items[0].sellIn).to.equal(-1);
-    });
+  // Test for generic item 'foo'
+  it('should foo', function() {
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal('foo');
+    expect(items[0].quality).to.equal(0);
+    expect(items[0].sellIn).to.equal(-1);
+  });
 
-    // Test for generic item 'bar'
-    // Quality degrades by 1 per day while sellIn is greater than 0
-    // Once its past its sell by date quality degrades by 2
-    it('should bar', function() {
-        const gildedRose = new GildedRose([ new Item('bar', 5, 15) ]);
-        let days = 5;
-        let expectedQuality = 15;
+  // Test for generic item 'bar'
+  // Quality degrades by 1 per day while sellIn is greater than 0
+  // Once its past its sell by date quality degrades by 2
+  it('should bar', function() {
+    const gildedRose = new GildedRose([new Item('bar', 5, 15)]);
+    let days = 5;
+    let expectedQuality = 15;
 
-        while (days > 0) {
-            const items = gildedRose.updateQuality();
-            expectedQuality--;
-            expect(items[0].quality).to.equal(expectedQuality);
-            days--;
-        }
+    while (days > 0) {
+      const items = gildedRose.updateQuality();
+      expectedQuality--;
+      expect(items[0].quality).to.equal(expectedQuality);
+      days--;
+    }
 
-        while (days > -5) {
-            const items = gildedRose.updateQuality();
-            expectedQuality = expectedQuality - 2;
-            expect(items[0].quality).to.equal(expectedQuality);
-            days--;
-        }
+    while (days > -5) {
+      const items = gildedRose.updateQuality();
+      expectedQuality = expectedQuality - 2;
+      expect(items[0].quality).to.equal(expectedQuality);
+      days--;
+    }
 
-        const updateAgain = gildedRose.updateQuality();
-        expect(updateAgain[0].quality).to.equal(0);
-    });
+    const updateAgain = gildedRose.updateQuality();
+    expect(updateAgain[0].quality).to.equal(0);
+  });
 
-    //
-    // Test for 'Aged Brie'
-    // Every day it gets older Quality increases by 2
-    // 
-    it('should Aged Brie', function() {
-        const agedBrie = new Item('Aged Brie', 5, 0);
-        const gildedRose = new GildedRose([ agedBrie ]);
+  //
+  // Test for 'Aged Brie'
+  // Every day it gets older Quality increases by 2
+  //
+  it('should Aged Brie', function() {
+    const agedBrie = new Item('Aged Brie', 5, 0);
+    const gildedRose = new GildedRose([agedBrie]);
 
-        let days = 5;
-        let expectedQuality = 0;
+    let days = 5;
+    let expectedQuality = 0;
 
-        while (days > 0) {
-            const items = gildedRose.updateQuality();
-            expectedQuality++;
-            expect(items[0].quality).to.equal(expectedQuality);
-            days--;
-        }
+    while (days > 0) {
+      const items = gildedRose.updateQuality();
+      expectedQuality++;
+      expect(items[0].quality).to.equal(expectedQuality);
+      days--;
+    }
 
-        
-        while (days > -25) {
-            const items = gildedRose.updateQuality();
-            expectedQuality = expectedQuality + 2;
+    while (days > -25) {
+      const items = gildedRose.updateQuality();
+      expectedQuality = expectedQuality + 2;
 
-            if (expectedQuality > 50) {
-                expectedQuality = 50;
-            }
-            
-            expect(items[0].quality).to.equal(expectedQuality);
-            days--;
-        }
+      if (expectedQuality > 50) {
+        expectedQuality = 50;
+      }
 
-    });
+      expect(items[0].quality).to.equal(expectedQuality);
+      days--;
+    }
+  });
 
-    //
-    // Test for 'Backstage passes to a TAFKAL80ETC concert'
-    // Quality goes up by 1 each day when the Sellin is greater than 10.
-    // Quality increases by 2 when there are 10 days or less 
-    // Quality increases by 3 when there are 5 days or less
-    // Quality drops to 0 after the concert.
-    // 
-    it('should Backstage passes quality increases', function() {
-        const backstagePassItem = new Item(backstagePass, 15, 1);
-        const gildedRose = new GildedRose([ backstagePassItem ]);
-        
-        let days = 15;
-        let expectedQuality = 1;
+  //
+  // Test for 'Backstage passes to a TAFKAL80ETC concert'
+  // Quality goes up by 1 each day when the Sellin is greater than 10.
+  // Quality increases by 2 when there are 10 days or less
+  // Quality increases by 3 when there are 5 days or less
+  // Quality drops to 0 after the concert.
+  //
+  it('should Backstage passes quality increases', function() {
+    const backstagePassItem = new Item(backstagePass, 15, 1);
+    const gildedRose = new GildedRose([backstagePassItem]);
 
-        while (days > 10) {
-            const items = gildedRose.updateQuality();
-            expectedQuality++;
-            expect(items[0].quality).to.equal(expectedQuality);
-            days--;
-        }
+    let days = 15;
+    let expectedQuality = 1;
 
-        while (days > 5) {
-            const items = gildedRose.updateQuality();
-            expectedQuality = expectedQuality + 2;
-            expect(items[0].quality).to.equal(expectedQuality);
-            days--;
-        }
+    while (days > 10) {
+      const items = gildedRose.updateQuality();
+      expectedQuality++;
+      expect(items[0].quality).to.equal(expectedQuality);
+      days--;
+    }
 
-        while (days > 0) {
-            const items = gildedRose.updateQuality();
-            expectedQuality = expectedQuality + 3;
-            expect(items[0].quality).to.equal(expectedQuality);
-            days--;
-        }
+    while (days > 5) {
+      const items = gildedRose.updateQuality();
+      expectedQuality = expectedQuality + 2;
+      expect(items[0].quality).to.equal(expectedQuality);
+      days--;
+    }
 
-        const updateAgain = gildedRose.updateQuality();
-        expect(updateAgain[0].name).to.equal(backstagePass);
-        expect(updateAgain[0].quality).to.equal(0);
-    });
+    while (days > 0) {
+      const items = gildedRose.updateQuality();
+      expectedQuality = expectedQuality + 3;
+      expect(items[0].quality).to.equal(expectedQuality);
+      days--;
+    }
 
-    //
-    // Test for 'Sulfuras, Hand of Ragnaros'
-    // Every day it gets older Quality increases by 2
-    // 
-    it('should Sulfuras, Hand of Ragnaros always be quality 80', function() {
-        const sulfurasItem = new Item(sulfuras, 1, 80);
-        const gildedRose = new GildedRose([ sulfurasItem ]);
-        
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal(sulfuras);
-        expect(items[0].quality).to.equal(80);
+    const updateAgain = gildedRose.updateQuality();
+    expect(updateAgain[0].name).to.equal(backstagePass);
+    expect(updateAgain[0].quality).to.equal(0);
+  });
 
-        const items2 = gildedRose.updateQuality();
-        expect(items2[0].name).to.equal(sulfuras);
-        expect(items2[0].quality).to.equal(80);
-    });
+  //
+  // Test for 'Sulfuras, Hand of Ragnaros'
+  // Every day it gets older Quality increases by 2
+  //
+  it('should Sulfuras, Hand of Ragnaros always be quality 80', function() {
+    const sulfurasItem = new Item(sulfuras, 1, 80);
+    const gildedRose = new GildedRose([sulfurasItem]);
 
-    // Test for 'Conjured Mana Cake'
-    // Quality degrades by 2 per day while sellIn is greater than 0
-    // Once its past its sell by date quality degrades by 4
-    it('should Conjured Mana Cake', function() {
-        const gildedRose = new GildedRose([ new Item('Conjured Mana Cake', 5, 16) ]);
-        let days = 5;
-        let expectedQuality = 16;
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal(sulfuras);
+    expect(items[0].quality).to.equal(80);
 
-        while (days > 0) {
-            const items = gildedRose.updateQuality();
-            expectedQuality = expectedQuality - 2;
-            expect(items[0].quality).to.equal(expectedQuality);
-            days--;
-        }
+    const items2 = gildedRose.updateQuality();
+    expect(items2[0].name).to.equal(sulfuras);
+    expect(items2[0].quality).to.equal(80);
+  });
 
-        while (days > -5) {
-            const items = gildedRose.updateQuality();
-            expectedQuality = expectedQuality - 4;
+  // Test for 'Conjured Mana Cake'
+  // Quality degrades by 2 per day while sellIn is greater than 0
+  // Once its past its sell by date quality degrades by 4
+  it('should Conjured Mana Cake', function() {
+    const gildedRose = new GildedRose([new Item('Conjured Mana Cake', 5, 16)]);
+    let days = 5;
+    let expectedQuality = 16;
 
-            if (expectedQuality < 0) {
-                expectedQuality = 0;
-            }
+    while (days > 0) {
+      const items = gildedRose.updateQuality();
+      expectedQuality = expectedQuality - 2;
+      expect(items[0].quality).to.equal(expectedQuality);
+      days--;
+    }
 
-            expect(items[0].quality).to.equal(expectedQuality);
-            days--;
-        }
-    });
+    while (days > -5) {
+      const items = gildedRose.updateQuality();
+      expectedQuality = expectedQuality - 4;
+
+      if (expectedQuality < 0) {
+        expectedQuality = 0;
+      }
+
+      expect(items[0].quality).to.equal(expectedQuality);
+      days--;
+    }
+  });
 });
